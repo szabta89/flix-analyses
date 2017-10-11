b5cfc2d46acc1c9f97ef0a672582387244dfd971
Tamás Szabó <tamas.szabo@itemis.de>
2017-10-11 23:08:31 +0200

    updated analysis and input data

--- measure.sh
+++ measure.sh
@@ -1,3 +1,41 @@
 #!/bin/sh
-#for i in {1..1}; do time java -jar flix.jar --optimize --verifier *.flix; done
-for i in {1..1}; do time java -jar flix.jar --optimize --verifier --print SUPT.VarPointsTo_FI,SUPT.FieldPointsTo_FI,SUPT.TargetHeapLocation_FI *.flix; done
+
+echo ""
+echo "Google Truth"
+for i in {1..5}; do 
+	start_time=$(ruby -e 'puts (Time.now.to_f * 1000).to_i')
+	java -jar flix.jar --verifier A_*.flix I_TRUTH.flix --timeout 5min --threads 8
+	end_time=$(ruby -e 'puts (Time.now.to_f * 1000).to_i')
+	duration=$((end_time - start_time))
+	echo "$duration"
+done
+
+echo ""
+echo "Google Gson"
+for i in {1..5}; do 
+	start_time=$(ruby -e 'puts (Time.now.to_f * 1000).to_i')
+	java -jar flix.jar --verifier A_*.flix I_GSON.flix
+	end_time=$(ruby -e 'puts (Time.now.to_f * 1000).to_i')
+	duration=$((end_time - start_time))
+	echo "$duration"
+done
+
+echo ""
+echo "PostgreSQL JDBC"
+for i in {1..5}; do 
+	start_time=$(ruby -e 'puts (Time.now.to_f * 1000).to_i')
+	java -jar flix.jar --verifier A_*.flix I_POSTGRESQL.flix
+	end_time=$(ruby -e 'puts (Time.now.to_f * 1000).to_i')
+	duration=$((end_time - start_time))
+	echo "$duration"
+done
+
+echo ""
+echo "BerkeleyDB"
+for i in {1..5}; do 
+	start_time=$(ruby -e 'puts (Time.now.to_f * 1000).to_i')
+	java -jar flix.jar --verifier A_*.flix I_BERKELEYDB.flix
+	end_time=$(ruby -e 'puts (Time.now.to_f * 1000).to_i')
+	duration=$((end_time - start_time))
+	echo "$duration"
+done
